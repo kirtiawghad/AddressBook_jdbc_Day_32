dac332e1929b4d68c5a999e734bcd7a273de9128
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-09-16 15:20:14 +0530

    Added: Uc3

--- src/main/java/com/bridgelabz/AddressBookUsingJdbc.java
+++ src/main/java/com/bridgelabz/AddressBookUsingJdbc.java
@@ -53,4 +53,39 @@ public class AddressBookUsingJdbc {
         }
         return contactsCount;
     }
+
+    public static int getContactsByState(Statement stmt){
+        int contactsCount = 0;
+        String getPersonFromState = "select * from address_book where state = 'KA'";
+        try{
+            ResultSet rs1 = stmt.executeQuery(getPersonFromState);
+            while (rs1.next()) {
+                contactsCount++;
+                // Retrieve by column name
+                String firstName = rs1.getString("firstname");
+                String lastName = rs1.getString("lastname");
+                String address = rs1.getString("address");
+                String city = rs1.getString("city");
+                String state = rs1.getString("state");
+                int zip = rs1.getInt("zip");
+                String phoneNumber = rs1.getString("phoneNumber");
+                String email = rs1.getString("email");
+
+                // Display values
+                System.out.println("first name: " + firstName);
+                System.out.println("last name: " + lastName);
+                System.out.println("address: " + address);
+                System.out.println("city: " + city);
+                System.out.println("state: " + state);
+                System.out.println("zip: " + zip);
+                System.out.println("phone number: " + phoneNumber);
+                System.out.println("email: " + email);
+
+                System.out.println("---------------------------");
+            }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+        return contactsCount;
+    }
 }
