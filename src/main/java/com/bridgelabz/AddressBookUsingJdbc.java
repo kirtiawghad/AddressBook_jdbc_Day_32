904dd3ca6d5414720a8d75b4f6d6a9b13859cf43
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-09-16 15:17:14 +0530

    Added: Uc2

--- src/main/java/com/bridgelabz/AddressBookUsingJdbc.java
+++ src/main/java/com/bridgelabz/AddressBookUsingJdbc.java
@@ -18,4 +18,39 @@ public class AddressBookUsingJdbc {
         }
         return totalRowCount;
     }
+
+    public static int getContactsByCity(Statement stmt){
+        int contactsCount = 0;
+        String getPersonFromCity = "select * from address_book where city = 'banglore'";
+        try{
+            ResultSet rs1 = stmt.executeQuery(getPersonFromCity);
+            while (rs1.next()) {
+                contactsCount++;
+                // Retrieve by column name
+                String firstName = rs1.getString("firstname");
+                String lastName = rs1.getString("lastname");
+                String address = rs1.getString("address");
+                String city = rs1.getString("city");
+                String state = rs1.getString("state");
+                int zip = rs1.getInt("zip");
+                String phoneNumber = rs1.getString("phoneNumber");
+                String email = rs1.getString("email");
+
+                // Display values
+                System.out.println("first name: " + firstName);
+                System.out.println("last name: " + lastName);
+                System.out.println("address: " + address);
+                System.out.println("city: " + city);
+                System.out.println("state: " + state);
+                System.out.println("zip: " + zip);
+                System.out.println("phone number: " + phoneNumber);
+                System.out.println("email: " + email);
+
+                System.out.println("---------------------------");
+            }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+        return contactsCount;
+    }
 }
--- src/test/java/com/bridgelabz/AddressBookTest.java
+++ src/test/java/com/bridgelabz/AddressBookTest.java
@@ -11,7 +11,7 @@ import java.sql.Statement;
 public class AddressBookTest {
     static final String DB_URL = "jdbc:mysql://localhost:3306/address_book_service_db";
     static final String USER = "root";
-    static final String PASS = "root";
+    static final String PASS = "vinitkulkarni";
 
     @Test
     public void givenRetrieveQueryShouldReturnTotalRowsCount(){
@@ -27,5 +27,22 @@ public class AddressBookTest {
             System.out.println(e);
         }
         Assert.assertEquals(6,totalRowsCount);
+    }
+
+    @Test
+    public void givenSearchByCityQueryShouldReturnContactsCount(){
+        Connection conn = null;
+        Statement stmt = null;
+        int countContactByCity = 0;
+        try {
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.createStatement();
+
+            countContactByCity = AddressBookUsingJdbc.getContactsByCity(stmt);
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+        Assert.assertEquals(1,countContactByCity);
+
     }
 }
